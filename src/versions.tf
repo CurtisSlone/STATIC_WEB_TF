4b07ce1dbcc97c90eb98f7a6271aec2fae173f4e
Curtis Slone <curtisdslone@gmail.com>
2023-04-11 15:36:46 +0200

    Init

--- src/versions.tf
+++ src/versions.tf
@@ -11,12 +11,12 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-    resource_group_name = "ampe-eastus2-network-artifacts-dev-rg"
-    storage_account_name = "ampetfstatesafcd3jta2"
-    container_name = "ampetfdevscfcd3jta2"
-    key = "terraform.tfstate"
-  }
+  # backend "azurerm" {
+  #   resource_group_name = "ampe-eastus2-network-artifacts-dev-rg"
+  #   storage_account_name = "ampetfstatesafcd3jta2"
+  #   container_name = "ampetfdevscfcd3jta2"
+  #   key = "terraform.tfstate"
+  # }
 }
 
 provider "azurerm" {
