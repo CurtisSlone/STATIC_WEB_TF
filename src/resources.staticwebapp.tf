6f23317f686caae4822f47aafd6c6dd0aefaeb50
Curtis Slone <curtisdslone@gmail.com>
2023-04-12 12:20:03 +0200

    Updated DNS

--- src/resources.staticwebapp.tf
+++ src/resources.staticwebapp.tf
@@ -20,6 +20,20 @@ resource "azurerm_dns_zone" "dnszone" {
 }
 
 #
+# AzureRM DNS TXT Record
+#
+resource "azurerm_dns_txt_record" "domain-verification" {
+  name = "www.slonesecurity.com"
+  zone_name = azurerm_dns_zone.dnszone.name
+  resource_group_name = module.static-web-app.resource_group_name
+  ttl = 300
+
+  record {
+    value = azurerm_static_site.staticapp.default_host_name
+  }
+}
+
+#
 # Azurerm Static Web Page Custom DNS CNAME
 #
 resource "azurerm_dns_cname_record" "name" {
