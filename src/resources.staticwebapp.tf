0551384ddc97aa6ae08cb1fdc70428840c2060c5
Curtis Slone <curtisdslone@gmail.com>
2023-04-12 12:46:17 +0200

    Updated custom domain

--- src/resources.staticwebapp.tf
+++ src/resources.staticwebapp.tf
@@ -10,43 +10,21 @@ resource "azurerm_static_site" "staticapp" {
   sku_size = "Standard"
 }
 
-#
-# Azurerm Static Web Page Custom DNS Zone
-#
-
-resource "azurerm_dns_zone" "dnszone" {
-  depends_on = [
-    azurerm_static_site.staticapp
-  ]
-  name = "slonesecurity.com" # Root domain
-  resource_group_name = module.static-web-app.resource_group_name
-}
-
-#
-# AzureRM DNS TXT Record
-#
-# resource "azurerm_dns_txt_record" "domain-verification" {
-  
-#   name = "slonesecurity.com"
-#   zone_name = azurerm_dns_zone.dnszone.name
-#   resource_group_name = module.static-web-app.resource_group_name
-#   ttl = 300
-
-#   record {
-#     value = azurerm_static_site.staticapp.default_host_name
-#   }
-# }
 
 #
 # Azurerm Static Web Page Custom DNS CNAME
 #
 resource "azurerm_dns_cname_record" "name" {
-  depends_on = [
-    azurerm_dns_zone.dnszone
-  ]
+
   name = "www"
-  zone_name = azurerm_dns_zone.dnszone.name
+  zone_name = "slonesecurity.com"
   resource_group_name = module.static-web-app.resource_group_name
   ttl = 300
-  record = azurerm_static_site.staticapp.api_key
+  record = azurerm_static_site.staticapp.default_host_name
+}
+
+resource "azurerm_static_site_custom_domain" "name" {
+  static_site_id = azurerm_static_site.staticapp.id
+  domain_name = "${ azurerm_dns_cname_record.name }.${azurerm_dns_cname_record.zone_name}"
+  validation_type = "cname-delegation"
 }
