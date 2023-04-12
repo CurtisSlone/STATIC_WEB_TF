23fe53eff9c5aba3f1b473f6cd18b54dbd995b96
Curtis Slone <curtisdslone@gmail.com>
2023-04-12 11:52:34 +0200

    Added DNS

--- src/resources.staticwebapp.tf
+++ src/resources.staticwebapp.tf
@@ -7,4 +7,25 @@ resource "azurerm_static_site" "staticapp" {
   resource_group_name = module.static-web-app.resource_group_name
   location = var.location
   sku_tier = "Standard"
+  sku_size = "Standard"
+}
+
+#
+# Azurerm Static Web Page Custom DNS Zone
+#
+
+resource "azurerm_dns_zone" "dnszone" {
+  name = "slonesecurity.com" # Root domain
+  resource_group_name = module.static-web-app.resource_group_name
+}
+
+#
+# Azurerm Static Web Page Custom DNS CNAME
+#
+resource "azurerm_dns_cname_record" "name" {
+  name = "test"
+  zone_name = azurerm_dns_zone.dnszone.name
+  resouresource_group_name = module.static-web-app.resource_group_name
+  ttl = 300
+  record = "brave-wave-0f630260f.3.azurestaticapps.net"
 }
